22ac4ef5492e8999ba6819827292ee3c5126c7b3
MarcTM <marctorresmartinez@gmail.com>
2020-09-24 09:02:52 +0200

    Exercice 2. Make danceWorld() not global by declaring the variable: let dw

---
+++ js/core/core.js
@@ -0,0 +1,7 @@
+function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        fn();
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+} 
